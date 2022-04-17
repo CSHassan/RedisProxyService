295e741c0901d3766734969ee1b6f4d4bf5535eb
Hassan Liaquat <hassan_l@hotmail.ca>
2022-04-17 13:42:08 -0400

    added global expirey to LRU cache

---
+++ src/local_cache.py
@@ -0,0 +1,70 @@
+from collections import OrderedDict
+from email.policy import default
+import time
+from threading import Lock
+from envyaml import EnvYAML
+
+env = EnvYAML('./env.yaml')
+
+# we have two dict, cache and global expiry
+# cache takes in the key/value pair from redis
+#global expiry takes the key and current time
+class LRUCache:
+ 
+    # initialising
+    def __init__(self):
+        self.cache = OrderedDict()
+        self.capacity = env['CACHE.SIZE']
+        self.global_expiry = {}
+        self.lock = Lock()
+ 
+    # first we lock and check if the key is expired
+    # we return the value of the key
+    # that is queried in O(1) and return -1 if we
+    # don't find the key in out dict / cache.
+    # and also move the key to the end
+    # also refresing the expiry timeout
+    # to show that it was recently used.
+    def get(self, key: int):
+        with self.lock:
+            if self._has_expired(key):
+                self._delete(key)                
+            if key not in self.cache:
+                return -1
+            else:
+                self.cache.move_to_end(key)            
+                self.global_expiry[key] = time.time()
+                return self.cache[key]
+ 
+    # first, we add / update the key by conventional methods.
+    # And also move the key to the end to show that it was recently used.
+    # But here we will also check whether the length of our
+    # ordered dictionary has exceeded our capacity,
+    # If so we remove the first key (least recently used)
+    # also refresh the expiry time
+    def put(self, key: int, value: int):
+        with self.lock:
+            if len(self.cache) > self.capacity:
+                self.cache.popitem(last = False)         
+            self.cache[key] = value            
+            self.cache.move_to_end(key)
+            self.global_expiry[key] = time.time()            
+                        
+            
+    def _delete(self, key):
+        try:
+            del self.cache[key]
+            del self.global_expiry[key]
+        except KeyError:
+            return False
+        return True
+    
+    def _has_expired(self, key):
+        exp = self.global_expiry.get(key, -1)
+        current_time = time.time()             
+        active_time = current_time - exp
+        print(active_time)
+        if (exp is not None and active_time >=env['CACHE.EXPIRY']):            
+            return True
+        else:
+            return False 
--- src/redis_backend.py
+++ src/redis_backend.py
@@ -7,7 +7,7 @@ from local_cache import LRUCache
 env = EnvYAML('./env.yaml')
 
 #intialize the cache
-inmem_cache = LRUCache(env['CACHE.SIZE'])
+inmem_cache = LRUCache()
 
 #make a redis pool to reuse the connection instead of a new connection
 backing_redis =redis.ConnectionPool(host=env['REDIS.REDIS_HOST'],port=env['REDIS.REDIS_PORT'])
@@ -16,7 +16,7 @@ backing_redis =redis.ConnectionPool(host=env['REDIS.REDIS_HOST'],port=env['REDIS
 async def get_redis_value(redis_id: str):
     redis_client = redis.Redis(connection_pool=backing_redis, decode_responses=True)
     cache_value = check_local_cache(redis_id)
-    if(cache_value != 0):
+    if(cache_value != -1):
         return cache_value
     else:
         redis_value = await redis_client.get(redis_id)
@@ -31,11 +31,11 @@ def set_local_cache(redis_id: str,redis_value: str):
 def check_local_cache(redis_id: str):
     value = inmem_cache.get(redis_id)
     if( value != -1):
-         print("Found in local cache: " + value)
+         print("Found in local cache..")
          return value
     else:
         print("Not found in local cache,feching from redis...")
-        return 0
+        return -1
      
 
 
